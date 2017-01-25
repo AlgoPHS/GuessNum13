779b97ac3d062d4c6ee67c9030c5b14e2b7ff953
Marie <marie.k.petitjean@gmail.com>
2017-01-25 13:46:50 -0500

    Basic Outline

--- 13Guess.py
+++ 13Guess.py
@@ -1,1 +1,11 @@
-#this
+def main():
+    tries =0
+    while True:
+        print("What is your geuss?")
+        guess = input()
+        tries+=1
+        if tries<4:
+            print("stuff")
+        else:
+            break
+main()
