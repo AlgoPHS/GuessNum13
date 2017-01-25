7d31e69f55f83e1097e1c330ca1d89229e63e718
Tyler Chen <txchen123@gmail.com>
2017-01-25 14:36:42 -0500

    finished the assignment

--- 13Guess.py
+++ 13Guess.py
@@ -1,11 +1,20 @@
+import random
+number = random.randint(1,100)
 def main():
     tries =0
     while True:
-        print("What is your geuss?")
+        print("What is your guess?")
         guess = input()
         tries+=1
         if tries<4:
-            print("stuff")
+            if guess == number:
+                print("Well done. I'm so proud!")
+                print("%s %s" % ("The number was", number))
+                break
+            else:
+                print("Not quite, try again!")
         else:
+            print("You have failed 4 times. Sad!")
+            print("%s %s" % ("The number was", number))
             break
 main()
