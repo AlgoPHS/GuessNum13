ecf1662ef856f793cd7837d2714cb8e5d3b624bd
Tyler Chen <txchen123@gmail.com>
2017-01-25 14:52:18 -0500

    fixed according to ms. elia's high standards

--- 13Guess.py
+++ 13Guess.py
@@ -4,15 +4,17 @@ def main():
     tries =0
     while True:
         print("What is your guess?")
-        guess = input()
+        guess = int(input())
         tries+=1
         if tries<4:
             if guess == number:
                 print("Well done. I'm so proud!")
                 print("%s %s" % ("The number was", number))
                 break
-            else:
-                print("Not quite, try again!")
+            if guess > number:
+                print("Too high, try again!")
+            if guess < number:
+                print("Too low, try again!")
         else:
             print("You have failed 4 times. Sad!")
             print("%s %s" % ("The number was", number))
