879b06da4bf21a12217b2d4c742b40cc63d86282
Marie <marie.k.petitjean@gmail.com>
2017-01-27 11:03:54 -0500

    made it follow game description

--- 13Guess.py
+++ 13Guess.py
@@ -1,39 +1,34 @@
 # Guess a number 13
 # Algo 2017
-# Marie + Tyler
-
+# Marie + Tyler 
 import random
 def playGame():
-    number = random.randint(1,100)
+    number = random.randint(1,101)
     tries =0
     while True:
-        if tries<4:
-            print("What is your guess?")
-            guess = int(input())
-            tries+=1
-            if guess == number:
-                print("Well done. I'm so proud!")
-                print("%s %s" % ("The number was", number))
-                print("Would you like to play again?")
-                if input() == "n":
-                    break
-                else:
-                    number = random.randint(1,100)
-                    tries = 0
-            if guess > number:
-                print("Too high, try again!")
-            if guess < number:
-                print("Too low, try again!")
-        else:
-            print("You have failed 4 times. Sad!")
-            print("%s %s" % ("The number was", number))
+        print("What is your guess?")
+        guess = int(input())
+        tries+=1
+        if guess == number:
+            print("Well done. I'm so proud!")
+            print("The number was " + str(number))
+            print("It took you %s tries."%(tries))
             break
+        if guess > number:
+            print("Too high, try again!")
+        if guess < number:
+            print("Too low, try again!")
+        
 def main():
+    print("I have a number between 1 and 100.")
     playGame()
+    games =1
     while True:
+        print("You played %s games"%(games))
         print("Would you like to play again? (y/n)")
         answer = input()
         if answer == 'y':
+            games +=1
             playGame()
         else:
             break
